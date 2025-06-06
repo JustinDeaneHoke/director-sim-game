1709ab4b4b48334b932d34616499af82812ba6b2
JustinDeaneHoke <justinhokekw@gmail.com>
2025-06-06 16:05:33 -0500

    Add page exports and update routes

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Project } from './Project';
+export { default as Production } from './Production';
+export { default as Results } from './Results';
+export { default as Profile } from './Profile';
