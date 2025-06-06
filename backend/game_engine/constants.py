decdf764b1ae67443870fe6139ae823792649408
JustinDeaneHoke <justinhokekw@gmail.com>
2025-06-06 11:48:30 -0500

    Create constants.py

---
+++ backend/game_engine/constants.py
@@ -0,0 +1,70 @@
+# constants for Director simulation game
+
+# Genres for film, TV, and commercials
+GENRES = {
+    'film': [
+        'drama',
+        'comedy',
+        'thriller',
+        'action',
+        'horror',
+        'science fiction',
+        'fantasy',
+        'romance'
+    ],
+    'tv': [
+        'sitcom',
+        'drama series',
+        'reality',
+        'talk show',
+        'documentary'
+    ],
+    'commercial': [
+        'automotive',
+        'food and beverage',
+        'technology',
+        'fashion',
+        'public service'
+    ]
+}
+
+# Budget levels
+BUDGET_LEVELS = [
+    'micro',
+    'indie',
+    'studio',
+    'blockbuster'
+]
+
+# Standard cast and crew roles
+ROLES = [
+    'director',
+    'producer',
+    'screenwriter',
+    'cinematographer',
+    'editor',
+    'production designer',
+    'lead actor',
+    'supporting actor',
+    'composer'
+]
+
+# Awards used in the game
+AWARDS = [
+    'best picture',
+    'best director',
+    'best screenplay',
+    'best actor',
+    'best actress',
+    'audience choice',
+    'best visual effects'
+]
+
+# Default tone options
+TONE_OPTIONS = [
+    'gritty',
+    'comedic',
+    'inspirational',
+    'dark',
+    'lighthearted'
+]
